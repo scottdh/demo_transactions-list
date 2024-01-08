423dfa2a876b57f5dc7356d6a547f72f3b6e15e2
Scott Herrington <scott.herrington@me.com>
2024-01-08 13:37:55 +0000

    messed around with adding dated headers

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -3,5 +3,6 @@ const moment = require("moment");
 module.exports = (value) => {
   const dateObject = moment(value);
   // return `${dateObject.format("Do")} of ${dateObject.format("MMMM YYYY")}`;
-  return `${dateObject.format("DD MMMM YYYY")}`;
+
+  return `${dateObject.format("MMMM Do YYYY")}`;
 };
